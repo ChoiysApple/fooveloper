73f737047710c2a9fd9e8e31ad6734c158e0d33b
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-08-08 20:04:57 +0900

    respawn, portal

---
+++ Assets/Scenes/Scripts/DeathZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //플레이어 들어왔다 표시
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/Portal.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+
+    //컴포넌트
+    private SpriteRenderer sprite_render;
+
+    //원하는 씬
+    public string SceneName = "";
+
+    //플레이어 닿았나 체크
+    private bool check_player = false;
+
+
+    //초기화
+    private void Start()
+    {
+        check_player = false;
+    }
+
+    //업데이트
+    private void Update()
+    {
+        if (check_player)
+        {
+                //빈칸 아니면 씬 로드
+                if (SceneName.Trim() != "")
+                {
+                    SceneManager.LoadScene(SceneName);
+                }      
+        }
+    }
+
+    #region 충돌 체크
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //플레이어 들어왔다 표시
+        if (collision.CompareTag("Player"))
+        {
+            check_player = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //플레이어 나갔다 표시
+        if (collision.CompareTag("Player"))
+        {
+            check_player = false;
+        }
+    }
+
+    #endregion
+
+    //플레이어 닿음 여부 반환
+    public bool GetCheckPlayer()
+    {
+        return check_player;
+    }
+
+}
