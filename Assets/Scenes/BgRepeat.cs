67035a5bc8cc776f8f377d7c25296df97e36b342
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-08-08 13:29:19 +0900

    Background...?

---
+++ Assets/Scenes/BgRepeat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgRepeat : MonoBehaviour {
+
+    public float scrollSpeed = 1.2f;
+    private Material thisMaterial;
+
+    void Start()
+    {
+        thisMaterial = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        Vector2 newOffset = thisMaterial.mainTextureOffset;
+        newOffset.Set(newOffset.x + (scrollSpeed * Time.deltaTime), 0);
+        thisMaterial.mainTextureOffset = newOffset;
+    }
+}
