95d5bcc4df390a0a8b5e6bc918c8d82046bfb52f
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-08-08 18:37:43 +0900

    Movable Box

---
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+
+public class GameManager : MonoBehaviour {
+
+    public static int ingredients = 4;
+    public static int dishes = 2;
+
+    bool[] isGet = new bool[ingredients];
+    bool[] isCooked = new bool[dishes];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
